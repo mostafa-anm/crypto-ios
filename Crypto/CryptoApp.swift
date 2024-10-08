4878fd19cb527f2000b659a7326ed3b3a4046229
Mostafa Mohamed <mostafa.anm91@gmail.com>
2024-10-08 07:37:49 +0200

    Initial commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Mos on 08.10.24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
