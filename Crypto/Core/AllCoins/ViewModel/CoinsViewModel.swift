a96dd7b3a6f362d1fff886b7b81954cea2aca795
Mostafa Mohamed <mostafa.anm91@gmail.com>
2024-10-08 13:44:07 +0200

    Add simple error  handling

--- Crypto/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ Crypto/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class CoinsViewModel: ObservableObject {
     @Published var coin = ""
     @Published var price = ""
+    @Published var errorMessage: String?
 
     init() {
         fetchPrice(coin: "litecoin")
@@ -17,23 +18,36 @@ class CoinsViewModel: ObservableObject {
 
     func fetchPrice(coin: String) {
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
-
         guard let url = URL(string: urlString) else { return }
 
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else { return }
-            guard let jsonObject =
-                    try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
-            guard let value = jsonObject[coin] as? [String: Double] else { return }
-
-            guard let price = value["usd"] else {return }
-
             DispatchQueue.main.async {
+                if let error = error {
+                    print("DEBUG: \(error.localizedDescription)")
+                    self.errorMessage = error.localizedDescription
+                    return
+                }
+
+                guard let response = response as? HTTPURLResponse else {
+                    self.errorMessage = "Response is not HTTPResponse"
+                    return
+                }
+
+                guard response.statusCode == 200 else {
+                    self.errorMessage = "Response is \(response.statusCode)"
+                    return
+                }
+
+                guard let data = data else { return }
+                guard let jsonObject =
+                        try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+                guard let value = jsonObject[coin] as? [String: Double] else { return }
+
+                guard let price = value["usd"] else {return }
+
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
-
-            print("Recieved data: \(jsonObject)")
         }.resume()
     }
 }
