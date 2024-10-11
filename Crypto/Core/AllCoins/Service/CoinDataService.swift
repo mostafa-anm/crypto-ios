be48103234fac4f5d140c757e14631d16979a23f
Mostafa Mohamed <mostafa.anm91@gmail.com>
2024-10-11 14:55:09 +0200

    User URL components to better manager endpoints

--- Crypto/Core/AllCoins/Service/CoinDataService.swift
+++ Crypto/Core/AllCoins/Service/CoinDataService.swift
@@ -8,14 +8,52 @@
 import Foundation
 
 class CoinDataService: HTTPDataDownloader {
-    private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc"
 
     func fetchCoins() async throws -> [Coin] {
-        return try await fetchData(as: [Coin].self, endpoint: urlString)
+        guard let endpoint = allCoinsURLString else {
+            throw CoinAPIError.requestfailed(description: "Invalid URL")
+        }
+        return try await fetchData(as: [Coin].self, endpoint: endpoint)
     }
 
     func fetchCoinDetails(id: String) async throws -> CoinDetails? {
-        let urlString = "https://api.coingecko.com/api/v3/coins/\(id)?localization=false"
-        return try await fetchData(as: CoinDetails.self, endpoint: urlString)
+        guard let endpoint = coinDetailsURLString(id: id) else {
+            throw CoinAPIError.requestfailed(description: "Invalid URL")
+        }
+        return try await fetchData(as: CoinDetails.self, endpoint: endpoint)
+    }
+
+    private var baseURLComponents: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.coingecko.com"
+        components.path = "/api/v3/coins/"
+
+        return components
+    }
+
+    private var allCoinsURLString: String? {
+        var components = baseURLComponents
+        components.path += "markets"
+
+        components.queryItems = [
+            .init(name: "vs_currency", value: "usd"),
+            .init(name: "order", value: "market_cap_desc"),
+            .init(name: "per_page", value: "20"),
+            .init(name: "page", value: "1"),
+            .init(name: "price_change_percentage", value: "24h")
+        ]
+
+        return components.url?.absoluteString
+    }
+
+    private func coinDetailsURLString(id: String) -> String? {
+        var components = baseURLComponents
+        components.path += "\(id)"
+        components.queryItems = [
+            .init(name: "localization", value: "false")
+        ]
+
+        return components.url?.absoluteString
     }
 }
